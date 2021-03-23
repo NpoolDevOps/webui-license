f43e55e7a338e9d778b6bb3daafbd8f5da67b6b5
Zhao KK <kikakkz@hotmail.com>
2021-03-23 18:31:01 +0800

    Support to modify password

--- html/license/license-frame.vue
+++ html/license/license-frame.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <el-row>
-            <el-button type="info" v-on:click="onModifyPassword">修改密碼</el-button>
-            <el-button type="info" v-if="superUser">添加用戶</el-button>
-            <el-button type="info" v-if="superUser">修改授權信息</el-button>
-            <el-button type="info" v-if="superUser">修改設備狀態</el-button>
+            <el-button type="primary" @click="onModifyPassword">修改密碼</el-button>
+            <el-button type="primary" v-if="superUser">添加用戶</el-button>
+            <el-button type="primary" v-if="superUser">修改授權信息</el-button>
+            <el-button type="primary" v-if="superUser">修改設備狀態</el-button>
         </el-row>
        <el-table border stripe v-if="showUserList" :data="userList">
            <el-table-column prop="username" label="用戶名"></el-table-column>
@@ -18,8 +18,8 @@
            <el-table-column prop="create_time" label="上線時間"></el-table-column>
            <el-table-column prop="status" label="狀態"></el-table-column>
        </el-table>
-       <el-dialog title="functionName" :visible.sync="showDialog" center :append-to-body="true" :lock-scroll="false" width="30%">
-           <password-modifier v-if="functionModifyPassword"></password-modifier>
+       <el-dialog :title="functionName" :visible.sync="showDialog" center :append-to-body="true" :lock-scroll="false" width="30%" :show-close="false">
+           <password-modifier v-if="functionModifyPassword" @submit="submitModifyPassword" @cancel="cancelModifyPassword"></password-modifier>
            <user-adder v-if="functionModifyPassword"></user-adder>
            <auth-modifier v-if="functionModifyPassword"></auth-modifier>
            <device-modifier v-if="functionModifyPassword"></device-modifier>
@@ -27,11 +27,6 @@
     </div>
 </template>
 <script>
-import PasswordModifier from './password-modifier'
-import UserAdder from './user-adder'
-import AuthModifier from './auth-modifier'
-import DeviceModifier from './device-modifier'
-
 module.exports = {
     data () {
         return {
@@ -51,10 +46,10 @@ module.exports = {
         }
     },
     components: {
-        PasswordModifier,
-        UserAdder,
-        AuthModifier,
-        DeviceModifier,
+        'password-modifier':httpVueLoader('./password-modifier.vue'),
+        'auth-modifier':httpVueLoader('./auth-modifier.vue'),
+        'device-modifier':httpVueLoader('./device-modifier.vue'),
+        'user-adder':httpVueLoader('./user-adder.vue'),
     },
     created: function() {
         this.$on('get_license_list', this.getLicenseList);
@@ -117,6 +112,83 @@ module.exports = {
         modifyPassword: function() {
             this.showDialog = true;
             this.functionModifyPassword = true;
+        },
+        finishModify: function() {
+            this.showDialog = false;
+            this.functionModifyPassword = false;
+            this.functionAddUser = false;
+            this.functionModifyAuth = false;
+            this.functionModifyDeviceStatus = false;
+        },
+        submitModifyPassword: function(passwordInfo) {
+            this.finishModify();
+            if (passwordInfo.newPassword != newPassword.confirmPassword) {
+                ELEMENT.Notification({
+                        title: '密碼設置不符合規範',
+                        message: '新密碼與確認密碼不一致',
+			            type: 'error',
+                    })
+                return;
+            }
+            if (passwordInfo.oldPassword == passwordInfo.newPassword) {
+                ELEMENT.Notification({
+                        title: '密碼設置不符合規範',
+                        message: '新密碼與舊密碼一樣',
+			            type: 'error',
+                    })
+                return;
+            }
+            if (passwordInfo.newPassword == '') {
+                ELEMENT.Notification({
+                        title: '密碼設置不符合規範',
+                        message: '新密碼不能爲空',
+			            type: 'error',
+                    })
+                return;
+            }
+
+            let oldEncPassword = sha256(passwordInfo.oldPassword).substring(0, 12)
+            let newEncPassword = sha256(passwordInfo.newPassword).substring(0, 12)
+
+            let authCode = this.$cookies.get('authcode');
+            if (!authCode || authCode == '' || authCode == 'null') {
+                ELEMENT.Notification({
+                    title: '授權碼無效',
+                    message: '授權碼缺失: ' + authCode,
+                    type: 'error',
+                })
+                return;
+            }
+
+            axios({
+                url: 'https://auth.npool.top/api/v0/user/modifypwd',
+                method: 'post',
+                headers: {'Content-Type': 'application/json'},
+                data: {
+                    auth_code: authCode,
+                    new_password: newEncPassword,
+                    old_password: oldEncPassword,
+                },
+            }).then(function (response) {
+                let resp = response.data;
+
+                if (resp.code != 0) {
+                    ELEMENT.Notification({
+                        title: '修改密碼失敗',
+                        message: resp.msg,
+			            type: 'error',
+                    })
+                }
+            }).catch(function (error) {
+                ELEMENT.Notification({
+                        title: '修改密碼失敗',
+                        message: error.message,
+			            type: 'error',
+                    })
+            })
+        },
+        cancelModifyPassword: function() {
+            this.finishModify();
         }
     }
 }
---
+++ html/license/password-modifier.vue
@@ -0,0 +1,42 @@
+<template>
+    <el-form :model="passwordInfo" label-width="80px">
+        <el-form-item label="當前用戶">
+            <el-input v-model="passwordInfo.username"></el-input>
+        </el-form-item>
+        <el-form-item label="舊密碼">
+            <el-input v-model="passwordInfo.currentPassword"></el-input>
+        </el-form-item>
+        <el-form-item label="新密碼">
+            <el-input v-model="passwordInfo.newPassword"></el-input>
+        </el-form-item>
+        <el-form-item label="確認密碼">
+            <el-input v-model="passwordInfo.confirmPassword"></el-input>
+        </el-form-item>
+        <el-row>
+            <el-button type="primary" @click="onSubmit">提交</el-button>
+            <el-button type="info" @click="onCancel">取消</el-button>
+        </el-row>
+    </el-form>
+</template>
+<script>
+module.exports = {
+    data () {
+        return {
+            passwordInfo: {
+                username: '',
+                oldPassword: '',
+                newPassword: '',
+                confirmPassword: '',
+            }
+        }
+    },
+    methods: {
+        onSubmit: function () {
+            this.$emit('submit', this.passwordInfo)
+        },
+        onCancel: function() {
+            this.$emit('cancel')
+        }
+    }
+}
+</script>
